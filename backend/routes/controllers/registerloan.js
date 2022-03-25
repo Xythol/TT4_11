9b154e300436fc795e744348696e8005b3407567
Darren <darren.png.2018@smu.edu.sg>
2022-03-25 09:13:44 +0100

    Update registerloan.js

--- backend/routes/controllers/registerloan.js
+++ backend/routes/controllers/registerloan.js
@@ -3,14 +3,15 @@ const connection = require('../../utils/connection');
 
 const login = async (req, res, next) => {
     const {cust_id} = auth.decode_jwt(req.headers.access_token) 
-    const {loan_amount} = req.body
+    
     const con = await connection();
 
     const [rows, fields] = await con.execute('INSERT INTO `loan` (`loan_amount`)', {cust_id});
     if (rows.length > 0) {
       
       return res.status(200).json({balance: balance}}
-    const [men, man] = await con.execute('INSERT INTO `customerloan` (`CustomerId`, `LoanId`)', {loan_amount});
+    const {loan_amount} = req.body
+    const [rows, fields] = await con.execute('INSERT INTO `customerloan` (`CustomerId`, `LoanId`)', {loan_amount});
     if (rows.length > 0) {
       const balance = rows[0].balance;
         return res.status(200).json({balance: balance});
