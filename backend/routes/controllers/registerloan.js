79f8e5783c9a132b69e2077f586a35576ff930d7
Darren <darren.png.2018@smu.edu.sg>
2022-03-25 08:14:31 +0100

    Updeets

--- backend/routes/controllers/registerloan.js
+++ backend/routes/controllers/registerloan.js
@@ -4,8 +4,8 @@ const connection = require('../../utils/connection');
 const login = async (req, res, next) => {
     const CustomerId = auth.decode_jwt(req.headers.access_token)
     const con = await connection();
-    
-    const [rows, fields] = await con.execute('SELECT balance FROM customer WHERE CustomerId=?', [CustomerId]);
+
+    const [rows, fields] = await con.execute('INSERT INTO `customerloan` (`CustomerLoanId`, `CustomerId`, `LoanId`)', [CustomerId]);
     if (rows.length > 0) {
       const balance = rows[0].balance;
         return res.status(200).json({balance: balance});
