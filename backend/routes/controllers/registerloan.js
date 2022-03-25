b3a6895517e57912081c993bb3415708037507b0
Darren <darren.png.2018@smu.edu.sg>
2022-03-25 09:50:37 +0100

    Update registerloan.js

--- backend/routes/controllers/registerloan.js
+++ backend/routes/controllers/registerloan.js
@@ -2,14 +2,15 @@ const auth = require('../../utils/auth');
 const connection = require('../../utils/connection');
 
 const login = async (req, res, next) => {
-    const {cust_id} = auth.decode_jwt(req.headers.access_token) 
+    const [cust_id] = auth.decode_jwt(req.headers.access_token) 
     
     const con = await connection();
 
-    const [___, _] = await con.execute('INSERT INTO `loan` (`loan_amount`) VALUES (?)', {cust_id});
+    result = await con.execute('INSERT INTO `loan` (`loan_amount`) VALUES (?)', [cust_id]);
     
-    const {loan_amount} = req.body
-    const [rowsv2, __] = await con.execute('INSERT INTO `customerloan` (`CustomerId`, `LoanId`) VALUES (?)', {loan_amount});
+    const [loan_amount] = req.body.loan_amount
+    const [LoanId] = req.body.LoanId
+    const [rowsv2, __] = await con.execute('INSERT INTO `customerloan` (`CustomerId`, `LoanId`) VALUES (?, ?)', [loan_amount, result[0].insertId]);
     if (rowsv2.length > 0) {
       return res.status(200).json();
 
